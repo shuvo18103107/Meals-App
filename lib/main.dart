1f581133e01cc04d5a7b9be06cde2046e5323ed9
shuvo18103107 <18103107@iubat.edu>
2021-02-26 22:19:28 +0600

    Adding base data and file setup

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyHomePage());
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DeliMeals'),
+      ),
+      body: Center(
+        child: Text('Navigation Time'),
+      ),
+    );
+  }
+}
