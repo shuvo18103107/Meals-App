047cab11affe4063a0dcb2f4d0c86c3cbdb2766e
shuvo18103107 <18103107@iubat.edu>
2021-03-01 23:05:04 +0600

    Adding a bottom tabbar

--- lib/screens/favourite_screen.dart
+++ lib/screens/favourite_screen.dart
@@ -4,8 +4,6 @@ class FavouriteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // jehutu favourite screen r category sceen ekhn tabbar er controlle tai eta tavview e just content dekhabe alada kore scafold use kore appbar ba body er drkr nai
-    return Center(
-      child: Text('facourite Screen'),
-    );
+    return Center(child: Text('Favourites'));
   }
 }
--- lib/screens/tabs_Screen.dart
+++ lib/screens/tabs_Screen.dart
@@ -8,43 +8,58 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(), 'title': 'Categories',
+      // if want some action
+      // 'action': FlatButton() in that way we can add dynamically
+    },
+    {'page': FavouriteScreen(), 'title': 'Your Favourites'}
+  ];
+
+  int _selectedPageIndex = 0;
+// now we get which item i tapped
+  void _selectpage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    //if tab on the top
-    return DefaultTabController(
-        initialIndex: 0,
+    // if we use tabbar on the bottom
 
-        // how many tabs i want define by length , one is favourite and another one is all categories
-        // defaultabcontroller and the tabbar are automatically connected by flutter behind the scene,so defaulttabcontroller can automatically detect which tab you are selecting and then show the write content for the tab
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        // bottomnavigationbar e manually kaj koraite hobe which tab user select and the content also
 
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Meals'),
-            // here we define tab bottom widget
-            bottom: TabBar(
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.category),
-                  text: 'Categories',
-                ),
-                Tab(
-                  icon: Icon(Icons.star),
-                  text: 'Favourites',
-                )
-              ],
-            ),
-          ),
-          body:
-              // if i choose 1st tab then the first tab content will show in tabvarview ,vice versa
+        onTap:
+            _selectpage, //in ontap flutter automatically gives the index and from this we understood which item is selected
+        backgroundColor: Theme.of(context).primaryColor,
+        selectedItemColor: Theme.of(context).accentColor,
+        unselectedItemColor: Colors.white,
+        //current kon item ta select kora ace tar ekta identification dorkar
+        //currentindex tell bottomnavigationbar which tap is actually selected
+        currentIndex: _selectedPageIndex,
 
-              TabBarView(
-            children: [
-              // note: order of the tab should be match with the order of tabverview children
-
-              CategoriesScreen(), FavouriteScreen()
-            ],
-          ),
-        ));
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+              // change some style if we use shifting
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.category),
+              label: 'Category'),
+          BottomNavigationBarItem(
+              // change some style if we use shifting
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.star),
+              label: 'Favourites'),
+        ],
+      ),
+    );
   }
 }
